4894d36f3c1c4758cd3b7caf21048b7c034b1a8b
Shubham Kumar <shubham@glassfrog.design>
2024-06-06 06:58:38 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,38 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
-import "../../public/globals.css"
+import "../../public/globals.css";
+import Head from "next/head";
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
-
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '293918380265545');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=293918380265545&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
