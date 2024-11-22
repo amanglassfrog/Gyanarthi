d2fb0806c1052423bcf3d935d5771440ef44ef0b
Shubham Kumar <shubham@glassfrog.design>
2024-11-22 11:04:12 +0530

    update

--- src/Components/VideoPopup.jsx
+++ src/Components/VideoPopup.jsx
@@ -7,7 +7,7 @@ const VideoPopup = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setPopupVisible(false);
-    }, 15000); // Automatically hide popup after 15 seconds
+    }, 120000); // Automatically hide popup after 15 seconds
     return () => clearTimeout(timer);
   }, []);
 
