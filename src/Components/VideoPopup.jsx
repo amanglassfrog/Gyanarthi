13ebec4d77deeb2917e7f7bf7611dc14143f41df
Shubham Kumar <shubham@glassfrog.design>
2024-11-23 10:11:41 +0530

    changes

--- src/Components/VideoPopup.jsx
+++ src/Components/VideoPopup.jsx
@@ -14,7 +14,7 @@ const VideoPopup = () => {
   return (
     isPopupVisible && (
       <div className="fixed inset-0 bg-black bg-opacity-70 z-50 flex justify-center items-center">
-        <div className="relative w-2/3 h-2/3 flex justify-center items-center">
+        <div className="relative w-full h-full p-2 md:p-0 md:w-2/3 md:h-2/3 flex justify-center items-center">
           <button
             className="absolute top-4 right-4 bg-white text-black rounded-full px-4 py-2 hover:bg-gray-200"
             onClick={() => setPopupVisible(false)}
