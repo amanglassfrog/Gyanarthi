92ac25d90ffe67c24c42f9246d0cafb084e0bbd6
Shubham Kumar <shubham@glassfrog.design>
2024-11-25 10:25:52 +0530

    changes in seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,12 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "../../public/globals.css";
-import Head from "next/head";
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        
-      </head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   );
