a833ab1dd5908ef1b1e925726f8119eaf2e6a4d1
Shubham Kumar <shubham@glassfrog.design>
2024-12-23 11:50:58 +0530

    changes up

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,17 +2,17 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "../../public/globals.css";
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
-    const GA_TRACKING_ID = 'G-FVZEWPFVML';
+const GA_TRACKING_ID = 'G-FVZEWPFVML';
+    export const metadata = {
+    title: "Gyanarthi: Best Fashion, Media, and Arts College in Kashipur",
+    description: "Pursue your College Education at Gyanarthi Media College, Kumaun University, in Fashion, Media, Commerce, Fine Arts, and Animation Course in city of Kashipur.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
      <head>
-        <title>Gyanarthi: Best Fashion, Media, and Arts College in Kashipur</title>       
-        <meta
-          name="description"
-          content="Pursue your College Education at Gyanarthi Media College, Kumaun University, in Fashion, Media, Commerce, Fine Arts, and Animation Course in city of Kashipur."
-        />
+       
         <script
           dangerouslySetInnerHTML={{
             __html: `
