b9fed44f57b07a34f98eedf4668436de0fed360e
Shubham Kumar <shubham@glassfrog.design>
2024-12-23 15:53:11 +0530

    changes

--- src/app/contact/Layout.js
+++ src/app/contact/Layout.js
@@ -1,18 +1,24 @@
-import { Plus_Jakarta_Sans } from "next/font/google";
-
-const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
+import React from "react";
 export const metadata = {
-    title: "Contact Us at Gyanarthi Media College for Admissions & More",
-    description: "With Gyanarthi Media College, you get the best education in Fashion, Media, Commerce, Fine Arts, Animation and Design. Contact Us for Queries and More Today!",
+  title: " Contact Us at Gyanarthi Media College for Admissions & More",
+  description:
+    "With Gyanarthi Media College, you get the best education in Fashion, Media, Commerce, Fine Arts, Animation and Design. Contact Us for Queries and More Today!",
+  
 };
-export default function RootLayout({ children }) {
-    return (
 
 
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main>
+            {children}
+            
+      </main>
+    </>
+  );
+};
 
-        </html>
-    );
-}
+export default Layout;
+
+
 
