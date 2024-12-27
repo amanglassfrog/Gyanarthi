c66bbd6d5911608b47b52f744c2a4ea27512776e
Shubham Kumar <shubham@glassfrog.design>
2024-12-27 14:53:16 +0530

    changes

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -15,23 +15,7 @@ import Head from 'next/head';
 const Bloglist = () => {
     return (
         <>
-            <head>
-       
-                {/* <!-- Facebook Meta Tags --> */}
-<meta property="og:url" content="https://www.gyanarthimedia.com/blogs"/>
-<meta property="og:type" content="website"/>
-<meta property="og:title" content="Read More about Gyanarthi Media College Kashipur"/>
-<meta property="og:description" content="Check out the Gyanarthi Blog for more information on the Courses, Curriculum, Infrastructure, Faculty, Life at the College, Media Studies, and more"/>
-<meta property="og:image" content="https://opengraph.b-cdn.net/production/images/1feb2971-8388-4a47-9406-a63ba404ba19.jpg?token=eZpSGk5upaDtXbUzablA8QFXP1gCv6jfLErchJyjITY&height=186&width=270&expires=33258587912"/>
-{/* <!-- Twitter Meta Tags --> */}
-<meta name="twitter:card" content="summary_large_image"/>
-<meta property="twitter:domain" content="gyanarthimedia.com"/>
-<meta property="twitter:url" content="https://www.gyanarthimedia.com/blogs"/>
-<meta name="twitter:title" content="Read More about Gyanarthi Media College Kashipur"/>
-<meta name="twitter:description" content="Check out the Gyanarthi Blog for more information on the Courses, Curriculum, Infrastructure, Faculty, Life at the College, Media Studies, and more"/>
-<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/1feb2971-8388-4a47-9406-a63ba404ba19.jpg?token=eZpSGk5upaDtXbUzablA8QFXP1gCv6jfLErchJyjITY&height=186&width=270&expires=33258587912"/>
-      
-      </head>
+            
             <Header />
             <div className="flex flex-wrap py-8 px-0 md:px-12 sm:px-24 pr-0 items-center justify-center">
                 <div className='container flex flex-col md:flex-row sm:flex-row items-center'>
