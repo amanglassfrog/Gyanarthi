88d6108f3a643728a9f2b11fbabaebed50116fe8
Shubham Kumar <shubham@glassfrog.design>
2025-01-03 10:37:49 +0530

    changes update

---
+++ src/app/8-upsides-of-mass-communication-and-journalism/layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+export const metadata = {
+  title: "upsides of mass communication and journalism",
+  description:
+    "Journalism and mass communication play interconnected roles in disseminating information to the broader audience. They enhance creativity and critical thinking. ",
+  alternates: {
+    canonical: "https://www.gyanarthimedia.com/8-upsides-of-mass-communication-and-journalism",
+  },
+  
+};
+
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      
+            {children}
+
+    </>
+  );
+};
+
+export default Layout;
+
+
+
+
+
+
---
+++ src/app/how-does-bfa-boost-your-creativity/layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+export const metadata = {
+  title: "Can Bachelor of Fine Arts  Boost Your Creativity?",
+  description:
+    "Unleash your creativity with a Bachelor of Fine Arts (BFA) degree. Explore art and sculpture. Develop problem-solving skills and turn ideas into reality.",
+  alternates: {
+    canonical: "https://www.gyanarthimedia.com/how-does-bfa-boost-your-creativity",
+  },
+  
+};
+
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      
+            {children}
+
+    </>
+  );
+};
+
+export default Layout;
+
+
+
+
+
+
