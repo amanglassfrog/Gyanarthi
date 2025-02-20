1531b7c9f9be170e489313067805e44477fd9874
manoj7417 <manoj@glassfrog.design>
2025-02-20 16:53:17 +0530

    Added new logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import logo from "../../../public/GyanarthiLogo.png";
+import logo from "../../../public/newlogo.png";
 import Image from "next/image";
 
 const Header = () => {
